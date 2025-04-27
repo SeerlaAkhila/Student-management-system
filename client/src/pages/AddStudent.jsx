472493377aba0aff4693589111abaa0e70123868
SeerlaAkhila <161214220+SeerlaAkhila@users.noreply.github.com>
2025-04-27 06:42:03 +0530

    Update AddStudent.jsx

--- client/src/pages/AddStudent.jsx
+++ client/src/pages/AddStudent.jsx
@@ -36,7 +36,7 @@ const AddStudent = () => {
     };
 
     // Updated URL with Render backend URL
-    axios.post('http://localhost:5000/students', formattedStudent)
+    axios.post(`${process.env.REACT_APP_BACKEND_URL}/students`, formattedStudent)
       .then(() => {
         toast.success('Student added successfully!');
         setTimeout(() => navigate('/students'), 2000); // delay to show toast
